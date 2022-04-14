552544870d0552315113bf3ebdcbddfb563becb7
peihwee <puaywee@gmail.com>
2022-04-15 00:46:07 +0800

    add speech

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Speech from './components/Speech.js';
 
 function App() {
+  var objSpeech = new Speech();
+  
+  objSpeech.init();
+
   return (
     <div className="App">
-      
+      <button onClick={() => objSpeech.speak("Hello World")}>Hello</button>
     </div>
   );
 }
